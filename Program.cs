85907e0226a7c674ab2f8e6d336259adac881fa9
akbararyo <akbararyo@gmail.com>
2019-04-02 11:51:16 +0700

    Method1

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarasi variabel
+            int hasil;
+
+            //membuat objek dari class Calculator
+            Calculator calculator = new Calculator();
+
+            //panggil method Penjumlah
+            hasil = calculator.Penjumlah(10, 2);
+            CetakHasil("Hasil penjumlahan: " + hasil);
+
+            //panggil method Penjumlah dg 3 parameter
+            hasil = calculator.Penjumlah(10, 2, 3);
+            CetakHasil("Hasil penjumlahan: " + hasil);
+
+            //panggil method Pengurangan
+            hasil = calculator.Pengurangan(7, 2);
+            CetakHasil("Hasil pengurangan: " + hasil);
+
+            //panggil static method, langsung dari nama classnya
+            hasil = Calculator.Perkalian(5, 2);
+            CetakHasil("Hasil perkalian: " + hasil);
+
+            Console.ReadKey();
+        }
+
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+
+    }
+}
